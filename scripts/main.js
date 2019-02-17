ffe24fd8c0afc58910e6d05b999b2e724a9987e2
Huan Ming Liao <hmliao14@gmail.com>
2019-02-17 14:44:26 -0800

    initial setup of boilerplates and linking files

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+function autorun()
+{
+  console.log('script is loaded');
+}
+if (document.addEventListener) document.addEventListener("DOMContentLoaded", autorun, false);
+else if (document.attachEvent) document.attachEvent("onreadystatechange", autorun);
+else window.onload = autorun;
