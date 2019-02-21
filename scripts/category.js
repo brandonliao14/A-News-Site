5332104e3afcd3879daff7d139f8d260ab90f2d9
Huan Ming Liao <hmliao14@gmail.com>
2019-02-20 20:26:32 -0800

    created and linked files to finance html

---
+++ scripts/category.js
@@ -0,0 +1,7 @@
+function autorun()
+{
+  console.log("category.js is loaded");
+}
+if (document.addEventListener) document.addEventListener("DOMContentLoaded", autorun, false);
+else if (document.attachEvent) document.attachEvent("onreadystatechange", autorun);
+else window.onload = autorun;
