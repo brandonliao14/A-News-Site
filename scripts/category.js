80a019aaaaa13f0cca829e2fb1e6bddd36c70aa8
Huan Ming Liao <hmliao14@gmail.com>
2019-02-20 22:57:02 -0800

    template and styled news entry

--- scripts/category.js
+++ scripts/category.js
@@ -6,8 +6,10 @@ function autorun()
   let category = window.location.pathname.split('/')[2].split('.')[0];
   let promise = readJSON(path);
   promise.then(function(data){
-    var news_feed = data[category];
-    console.log(news_feed);
+    console.log(data);
+    let main_body_content = document.getElementsByClassName('main_body_content')[0];
+    console.log(templateContents(data[category],category));
+    main_body_content.innerHTML = templateContents(data[category],category);
   }).then().catch(function(err){
     console.log(err);
   })
@@ -15,6 +17,34 @@ function autorun()
 if (document.addEventListener) document.addEventListener("DOMContentLoaded", autorun, false);
 else if (document.attachEvent) document.attachEvent("onreadystatechange", autorun);
 else window.onload = autorun;
+
+function templateContents(list_of_news,category){
+  let entries = "";
+  let curr_news;
+  for(var i =0; i < list_of_news.length; i++){
+    curr_news = list_of_news[i];
+    entries +=
+    `
+      <div class="row news_entry">
+        <div class="col-12 col-sm-3 col-md-4 col-lg-4">
+          <img src="${curr_news.image}" alt="Broken Image">
+        </div>
+        <div class="col-12 col-sm-9 col-md-8 col-lg-8">
+          <a href="${curr_news.link} class="font-weight-bold">${curr_news.title}</a>
+          <p class="text-muted">${curr_news.author ? curr_news.author + " - " : "" } ${curr_news.date}</p>
+        </div>
+      </div>
+    `;
+    console.log(entries);
+  }
+  console.log(entries);
+  return `
+    <div>
+      <h4 class="text-capitalize">${category}</h4>
+      ${entries}
+    </div>
+  `
+}
 
 function readJSON(path) {
   return new Promise(function(resolve,reject){
